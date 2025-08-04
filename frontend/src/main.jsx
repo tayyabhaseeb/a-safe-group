8f909a06dbe8c58301513c9bd2b85eb9ef174431
“Tayyab <tayyabhaseeb5@gmail.com>
2025-08-04 14:09:35 +0100

    react with tailwind  setup

---
+++ frontend/src/main.jsx
@@ -0,0 +1,10 @@
+import React, { StrictMode } from "react"; // ✅ Add this line
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
